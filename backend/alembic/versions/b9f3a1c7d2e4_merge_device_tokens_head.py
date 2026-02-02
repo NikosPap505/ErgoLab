95f33a292843bfc15a964e1211f6907adff165af
NikosPap505 <papoutsis98@hotmail.com>
2026-02-02 20:48:48 +0200

    feat: Add migration script for merging device tokens

---
+++ backend/alembic/versions/b9f3a1c7d2e4_merge_device_tokens_head.py
@@ -0,0 +1,21 @@
+"""merge_device_tokens_head
+
+Revision ID: b9f3a1c7d2e4
+Revises: e650e02d28e3, add_device_tokens
+Create Date: 2026-02-02 20:45:00
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "b9f3a1c7d2e4"
+down_revision = ("e650e02d28e3", "add_device_tokens")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
